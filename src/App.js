132e43b77d445b8a05813277ded1621c89bfda1e
ShireeshaPanduru <121652807+ShireeshaPanduru@users.noreply.github.com>
2023-02-09 15:48:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './Components/style.css';
+import Main from './Components/main';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Main/>
+    </div>
+  );
+}
+
+export default App;
